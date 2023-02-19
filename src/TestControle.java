b94ee6e4464a35410fb90c9275797bbe80315736
Luis Felipe <vilella.luisfelipe@gmail.com>
2023-02-19 14:58:21 -0300

    Add setUp and tearDown to fix test smell evaluation

--- src/TestControle.java
+++ src/TestControle.java
@@ -1,70 +1,99 @@
 import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 
-class TestControle {
+public class TestControle {
+	
+	private Aluno alunoNaoVereficado;
+	private Aluno alunoComDebito;
+	private String raValido;
+	private Controle c;
+	private Livro livroVerificado1;
+	private Livro livroNaoVerificado1;
+	private Livro livroNaoVerificado2;
+
+	
+	@Before // setup()
+    public void before() throws Exception {
+        System.out.println("Setting it up!");
+        alunoNaoVereficado = new Aluno("10");
+        alunoComDebito = new Aluno("4");
+        raValido = "123";
+
+		c = new Controle();
+
+		livroVerificado1 = new Livro(1);
+		livroNaoVerificado1 = new Livro(2);  // 2 nao é verificado
+		livroNaoVerificado2 = new Livro(4);  // 4 nao é verificado
+    }
 
 	@Test
-	void testNaoEmprestaLivrosParaAlunoNaoVerificado() {
-		String ra = "10";
-		Aluno aluno = new Aluno(ra);
-		assertFalse(aluno.verficaAluno());
+	public void testNaoEmprestaLivrosParaAlunoNaoVerificado() {
+		assertFalse(this.alunoNaoVereficado.verficaAluno());
 
 		int[] prazos = {1,2,3};
-		Controle c = new Controle();
-		boolean retorno = c.emprestar(ra, prazos, prazos.length);
+		boolean retorno = c.emprestar(this.alunoNaoVereficado.RA, prazos, prazos.length);
+		assertFalse(retorno);
+	}
+	
+	@Test
+	public void testNaoEmprestaLivrosParaAlunoComDebito() {
+		assertFalse(alunoComDebito.verificaDebito());
+
+		int[] prazos = {1,2,3};
+		boolean retorno = c.emprestar(alunoComDebito.RA, prazos, prazos.length);
 		assertFalse(retorno);
 	}
 
 	@Test
-	void testNaoEmprestaLivrosParaAlunoComDebito() {
-		String ra = "4";
-		Aluno aluno = new Aluno(ra);
-		assertFalse(aluno.verificaDebito());
-
-		int[] prazos = {1,2,3};
-		Controle c = new Controle();
-		boolean retorno = c.emprestar(ra, prazos, prazos.length);
-		assertFalse(retorno);
-	}
-
-	@Test
-	void testNaoEmprestaLivroNaoVerificado() {
-		String ra = "123";
+	public void testNaoEmprestaLivroNaoVerificado() {
 		int[] prazos = {1,2,4};  // 2 e 4 nao é verificado
-		Livro livro0 = new Livro(1);
-		assertTrue(!livro0.verificaLivro());
-		Livro livro1 = new Livro(2);
-		assertFalse(!livro1.verificaLivro());
-		Livro livro2 = new Livro(4);
-		assertFalse(!livro2.verificaLivro());
+		assertTrue(!livroVerificado1.verificaLivro());
+		assertFalse(!livroNaoVerificado1.verificaLivro());
+		assertFalse(!livroNaoVerificado2.verificaLivro());
 		
-		Controle c = new Controle();
-		boolean retorno = c.emprestar(ra, prazos, prazos.length);
+		boolean retorno = c.emprestar(raValido, prazos, prazos.length);
 		assertTrue(retorno);  // emprestou só 1 livro
 	}
 
 	@Test
-	void testNaoEmprestaNenhumLivroNaoVerificado() {
-		String ra = "123";
+	public void testNaoEmprestaNenhumLivroNaoVerificado() {
 		int[] prazos = {2,4};  // 2 e 4 nao é verificado
-		Livro livro1 = new Livro(2);
-		assertFalse(!livro1.verificaLivro());
-		Livro livro2 = new Livro(4);
-		assertFalse(!livro2.verificaLivro());
+		assertFalse(!livroNaoVerificado1.verificaLivro());
+		assertFalse(!livroNaoVerificado2.verificaLivro());
 		
-		Controle c = new Controle();
-		boolean retorno = c.emprestar(ra, prazos, prazos.length);
+		boolean retorno = c.emprestar(raValido, prazos, prazos.length);
 		assertFalse(retorno);
 	}
 
 	@Test
-	void testEmprestaLivros() {
-		String ra = "123";
+	public void testEmprestaLivros() {
 		int[] prazos = {1,3,5,6};
-		Controle c = new Controle();
-		boolean retorno = c.emprestar(ra, prazos, prazos.length);
+		boolean retorno = c.emprestar(raValido, prazos, prazos.length);
 		assertTrue(retorno);
 	}
+	
+    @After // tearDown()
+    public void after() throws Exception {
+        System.out.println("Running: tearDown");
+        alunoNaoVereficado = null;
+        assertNull(alunoNaoVereficado);
+        alunoComDebito = null;
+        assertNull(alunoComDebito);
+        raValido = null;
+        assertNull(raValido);
+
+        c = null;
+        assertNull(alunoComDebito);
+
+		livroVerificado1 = null;
+        assertNull(livroVerificado1);
+		livroNaoVerificado1 = null;
+        assertNull(livroNaoVerificado1);
+		livroNaoVerificado2 = null;
+        assertNull(livroNaoVerificado2);
+    }
 }
--- src/TestEmprestimo.java
+++ src/TestEmprestimo.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -7,77 +8,118 @@ import java.util.List;
 
 import java.lang.IllegalArgumentException;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
-class TestEmprestimo {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestEmprestimo {
+	
+	private Livro livro1;
+	private Livro livro2;
+	private Livro livro3;
+	private List<Livro> livros;
+	private List<Livro> livrosBatchOf2;
+	private List<Livro> livrosBatchOf3;
+	private List<Livro> livrosBatchOf7;  // Excede o limite
+	private Emprestimo emprestimo;
+
+	private Calendar calendar;
+	
+	@Before // setup()
+    public void before() throws Exception {
+        System.out.println("Setting it up!");
+		livro1 = new Livro(0);
+		livro2 = new Livro(1);
+		livro3 = new Livro(2);
+		livros = new ArrayList<Livro>();  
+
+		livrosBatchOf2 = new ArrayList<Livro>();  
+		livrosBatchOf2.add(livro1);
+		livrosBatchOf2.add(livro2);
+
+		livrosBatchOf3 = new ArrayList<Livro>();  
+		livrosBatchOf3.add(livro1);
+		livrosBatchOf3.add(livro2);
+		livrosBatchOf3.add(livro3);
+
+		livrosBatchOf7 = new ArrayList<Livro>();  
+		for(int i=0; i < 8;i++) {
+        	livrosBatchOf7.add(new Livro(i));
+        }
+
+		emprestimo = new Emprestimo();
+
+		calendar = Calendar.getInstance();
+
+	}
 
 	@Test
-	void testEmpresta1LivroNaoAplicaDataExtra() {
-		Livro l = new Livro(1);
-		List<Livro> livros = new ArrayList<Livro>();  
-		livros.add(l);
-		Emprestimo emprestimo = new Emprestimo();
+	public void testEmpresta1LivroNaoAplicaDataExtra() {
+		livros.add(livro2);
 		emprestimo.emprestar(livros);
 		
 		Item ultimoItem = emprestimo.item.get(0);
 
 		Date now = new Date(System.currentTimeMillis());
-		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(now);
 		calendar.add(Calendar.DAY_OF_MONTH, 2);  // 2 days
 		assertEquals(ultimoItem.dataDevolucao.getDay(), calendar.getTime().getDay());
 	}
 
 	@Test
-	void testEmpresta2LivroNaoAplicaDataExtra() {
-		List<Livro> livros = new ArrayList<Livro>();  
-        for(int i=0; i < 2;i++) {
-        	Livro l = new Livro(i);
-        	livros.add(l);
-        }
-        
-		Emprestimo emprestimo = new Emprestimo();
-		emprestimo.emprestar(livros);
+	public void testEmpresta2LivroNaoAplicaDataExtra() {
+		emprestimo.emprestar(livrosBatchOf2);
 		Item ultimoItem = emprestimo.item.get(1);
 
 		Date now = new Date(System.currentTimeMillis());
-		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(now);
 		calendar.add(Calendar.DAY_OF_MONTH, 2);  // 2 days
 		assertEquals(ultimoItem.dataDevolucao.getDay(), calendar.getTime().getDay());
 	}
 
 	@Test
-	void testEmpresta3LivroAplicaDataExtra() {
-		List<Livro> livros = new ArrayList<Livro>();  
-        for(int i=0; i < 3;i++) {
-        	Livro l = new Livro(i);
-        	livros.add(l);
-        }
-        
+	public void testEmpresta3LivroAplicaDataExtra() {
 		Emprestimo emprestimo = new Emprestimo();
-		emprestimo.emprestar(livros);
+		emprestimo.emprestar(livrosBatchOf3);
 		Item ultimoItem = emprestimo.item.get(2);
 
 		Date now = new Date(System.currentTimeMillis());
-		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(now);
 		calendar.add(Calendar.DAY_OF_MONTH, 5);  // 3 days
 		assertEquals(ultimoItem.dataDevolucao.getDay(), calendar.getTime().getDay());
 	}
 	
 	@Test
-	void testEmprestaMaisDe7LivrosExcedeLimite() {
-		List<Livro> livros = new ArrayList<Livro>();  
-        for(int i=0; i < 8;i++) {
-        	Livro l = new Livro(i);
-        	livros.add(l);
-        }
-		Emprestimo emprestimo = new Emprestimo();
+	public void testEmprestaMaisDe7LivrosExcedeLimite() {
 		IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			emprestimo.emprestar(livros);
+			emprestimo.emprestar(livrosBatchOf7);
 		});
 		Assertions.assertEquals("Excedeu limite maximo de 7 livros", thrown.getMessage());
 	}
 
+	@After // tearDown()
+    public void after() throws Exception {
+        System.out.println("Running: tearDown");
+		livro1 = null;
+        assertNull(livro1);
+		livro2 = null;
+        assertNull(livro2);
+		livros = null;
+        assertNull(livros);
+
+		livrosBatchOf2 = null;
+        assertNull(livrosBatchOf2);
+		livrosBatchOf3 = null;
+        assertNull(livrosBatchOf3);
+		livrosBatchOf7 = null; 
+        assertNull(livrosBatchOf7);
+
+		emprestimo = null;
+        assertNull(emprestimo);
+
+		calendar = null;
+        assertNull(calendar);
+    }
 }
